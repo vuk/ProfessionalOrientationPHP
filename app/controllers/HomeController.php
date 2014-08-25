e2a2641eb85ee594b55327fa74d03dfe6d8a7c32
Vuk Stanković <vuks89@gmail.com>
2014-08-25 11:51:00 +0200

    Initial commit

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,383 @@
+<?php
+
+class HomeController extends BaseController {
+
+        public function __construct(){
+                parent::__construct();
+                View::share('title', 'Profesionalna orijentacija');
+                if(!Session::get('id') && Input::get('id')){
+                        Session::put('id', Input::get('id'));
+                }
+        }
+
+	/*
+	|--------------------------------------------------------------------------
+	| Default Home Controller
+	|--------------------------------------------------------------------------
+	|
+	| You may wish to use controllers instead of, or in addition to, Closure
+	| based routes. That's great! Here is an example controller method to
+	| get you started. To route to this controller, just add the route:
+	|
+	|	Route::get('/', 'HomeController@showWelcome');
+	|
+	*/
+
+	public function getIndex(){
+
+                $ch = curl_init(); 
+                curl_setopt($ch, CURLOPT_URL, "http://localhost:8080/Professional_Orientation/api/municipalities"); 
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+                $output = curl_exec($ch); 
+                curl_close($ch);      
+                View::share('municipalities', json_decode($output));
+
+                $ch = curl_init(); 
+                curl_setopt($ch, CURLOPT_URL, "http://localhost:8080/Professional_Orientation/api/schools"); 
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+                $output = curl_exec($ch); 
+                curl_close($ch);      
+                View::share('schools', json_decode($output));
+
+                View::share('action', 'HomeController@postBasicInfo');
+		$this->layout           = View::make('basic');
+                $this->layout->head     = View::make('head');
+	}
+
+        public function postBasicInfo(){
+                
+        }
+
+	public function getPersonalityQuestions()
+	{
+                if(!isset($_SERVER['HTTP_REFERER'])){
+                        return Redirect::to(URL::action('HomeController@getIndex'));
+                }
+		$ch = curl_init(); 
+
+                // set url 
+                curl_setopt($ch, CURLOPT_URL, "http://localhost:8080/Professional_Orientation/api/personality"); 
+
+                //return the transfer as a string 
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+
+                // $output contains the output string 
+                $output = curl_exec($ch); 
+
+                // close curl resource to free up system resources 
+                curl_close($ch);      
+
+                View::share('json', json_decode($output));
+                View::share('question_type', 'u vezi sa osobinama ličnosti');
+                View::share('action', 'HomeController@postCalculatePersonality');
+		$this->layout 		= View::make('index');
+		$this->layout->head 	= View::make('head');
+	}
+
+	public function postCalculatePersonality(){
+		$realistic = 0;
+		$explorer = 0;
+		$artistic = 0;
+		$social = 0;
+		$entrepreneur = 0;
+		$conventional = 0;
+
+		$realistic_sum = 0;
+		$explorer_sum = 0;
+		$artistic_sum = 0;
+		$social_sum = 0;
+		$entrepreneur_sum = 0;
+		$conventional_sum = 0;
+
+		$ch = curl_init(); 
+
+                // set url 
+                curl_setopt($ch, CURLOPT_URL, "http://localhost:8080/Professional_Orientation/api/personality"); 
+
+                //return the transfer as a string 
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+
+                // $output contains the output string 
+                $output = curl_exec($ch); 
+        	       
+        	    $json = json_decode($output);
+
+                // close curl resource to free up system resources 
+                curl_close($ch);      
+
+                foreach($json as $q){
+                	if($q->personality_type_id == 1){
+                		$realistic ++;
+                		$realistic_sum += Input::get($q->id);
+                	}
+                	if($q->personality_type_id == 2){
+                		$explorer ++;
+                		$explorer_sum += Input::get($q->id);
+                	}
+                	if($q->personality_type_id == 3){
+                		$artistic ++;
+                		$artistic_sum += Input::get($q->id);
+                	}
+                	if($q->personality_type_id == 4){
+                		$social ++;
+                		$social_sum += Input::get($q->id);
+                	}
+                	if($q->personality_type_id == 5){
+                		$entrepreneur ++;
+                		$entrepreneur_sum += Input::get($q->id);
+                	}
+                	if($q->personality_type_id == 6){
+                		$conventional ++;
+                		$conventional_sum += Input::get($q->id);
+                	}
+                        Session::put($q->id, Input::get($q->id));
+                }
+
+                $realistic_calc = $realistic_sum/$realistic;
+                $explorer_calc = $explorer_sum/$explorer;
+                $artistic_calc = $artistic_sum/$artistic;
+                $social_calc = $social_sum/$social;
+                $entrepreneur_calc = $entrepreneur_sum/$entrepreneur;
+                $conventional_calc = $conventional_sum/$conventional;
+                Session::put('realistic', $realistic_calc);
+                Session::put('explorer', $explorer_calc);
+                Session::put('artistic', $artistic_calc);
+                Session::put('social', $social_calc);
+                Session::put('entrepreneur', $entrepreneur_calc);
+                Session::put('conventional', $conventional_calc);
+
+                return Redirect::to(URL::action('HomeController@getAreaQuestions'));
+	}
+
+	public function getAreaQuestions(){
+                if(!isset($_SERVER['HTTP_REFERER'])){
+                        return Redirect::to(URL::action('HomeController@getPersonalityQuestions'));
+                }
+                else{
+                        if(FALSE !== strpos($_SERVER['HTTP_REFERER'], 'personality-questions') && !Session::get('realistic')){
+                                return Redirect::to(URL::action('HomeController@getPersonalityQuestions'));
+                        }
+                        $ch = curl_init(); 
+
+                        // set url 
+                        curl_setopt($ch, CURLOPT_URL, "http://localhost:8080/Professional_Orientation/api/areas"); 
+
+                        //return the transfer as a string 
+                        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+
+                        // $output contains the output string 
+                        $output = curl_exec($ch); 
+
+                        // close curl resource to free up system resources 
+                        curl_close($ch);      
+
+                        View::share('json', json_decode($output));
+                        View::share('question_type', 'u vezi sa oblastima interesovanja');
+                        View::share('action', 'HomeController@postCalculateAreas');
+
+                        $this->layout           = View::make('index');
+                        $this->layout->head     = View::make('head');
+                }
+	}
+
+        public function postCalculateAreas(){
+                $administration = 0;
+                $security = 0;
+                $technology = 0;
+                $creativity = 0;
+                $culture = 0;
+                $literature = 0;
+                $science = 0;
+                $helping = 0;
+                $agriculture = 0;
+                $craftsmenship = 0;
+                $sports = 0;
+                $services = 0;
+                $management = 0;
+
+                $administration_sum = 0;
+                $security_sum = 0;
+                $technology_sum = 0;
+                $creativity_sum = 0;
+                $culture_sum = 0;
+                $literature_sum = 0;
+                $science_sum = 0;
+                $helping_sum = 0;
+                $agriculture_sum = 0;
+                $craftsmenship_sum = 0;
+                $sports_sum = 0;
+                $services_sum = 0;
+                $management_sum = 0;
+
+                $ch = curl_init(); 
+
+                // set url 
+                curl_setopt($ch, CURLOPT_URL, "http://localhost:8080/Professional_Orientation/api/areas"); 
+
+                //return the transfer as a string 
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+
+                // $output contains the output string 
+                $output = curl_exec($ch); 
+                       
+                $json = json_decode($output);
+
+                // close curl resource to free up system resources 
+                curl_close($ch);      
+
+                foreach($json as $q){
+                        if($q->question_type_id == 1){
+                                $administration ++;
+                                $administration_sum += Input::get($q->id);
+                        }
+                        if($q->question_type_id == 2){
+                                $security ++;
+                                $security_sum += Input::get($q->id);
+                        }
+                        if($q->question_type_id == 3){
+                                $technology ++;
+                                $technology_sum += Input::get($q->id);
+                        }
+                        if($q->question_type_id == 4){
+                                $creativity ++;
+                                $creativity_sum += Input::get($q->id);
+                        }
+                        if($q->question_type_id == 5){
+                                $culture ++;
+                                $culture_sum += Input::get($q->id);
+                        }
+                        if($q->question_type_id == 6){
+                                $literature ++;
+                                $literature_sum += Input::get($q->id);
+                        }
+                        if($q->question_type_id == 7){
+                                $science ++;
+                                $science_sum += Input::get($q->id);
+                        }
+                        if($q->question_type_id == 8){
+                                $helping ++;
+                                $helping_sum += Input::get($q->id);
+                        }
+                        if($q->question_type_id == 9){
+                                $agriculture ++;
+                                $agriculture_sum += Input::get($q->id);
+                        }
+                        if($q->question_type_id == 10){
+                                $craftsmenship ++;
+                                $craftsmenship_sum += Input::get($q->id);
+                        }
+                        if($q->question_type_id == 11){
+                                $sports ++;
+                                $sports_sum += Input::get($q->id);
+                        }
+                        if($q->question_type_id == 12){
+                                $services ++;
+                                $services_sum += Input::get($q->id);
+                        }
+                        if($q->question_type_id == 13){
+                                $management ++;
+                                $management_sum += Input::get($q->id);
+                        }
+                        Session::put($q->id, Input::get($q->id));
+                }
+
+                $administration_calc = $administration_sum/$administration;
+                $security_calc = $security_sum/$security;
+                $technology_calc = $technology_sum/$technology;
+                $creativity_calc = $creativity_sum/$creativity;
+                $culture_calc = $culture_sum/$culture;
+                $literature_calc = $literature_sum/$literature;
+                $science_calc = $science_sum/$science;
+                $helping_calc = $helping_sum/$helping;
+                $agriculture_calc = $agriculture_sum/$agriculture;
+                $craftsmenship_calc = $craftsmenship_sum/$craftsmenship;
+                $sports_calc = $sports_sum/$sports;
+                $services_calc = $services_sum/$services;
+                $management_calc = $management_sum/$management;
+                
+                Session::put('administration', $administration_calc);
+                Session::put('security', $security_calc);
+                Session::put('technology', $technology_calc);
+                Session::put('creativity', $creativity_calc);
+                Session::put('culture', $culture_calc);
+                Session::put('literature', $literature_calc);
+                Session::put('science', $science_calc);
+                Session::put('helping', $helping_calc);
+                Session::put('agriculture', $agriculture_calc);
+                Session::put('craftsmenship', $craftsmenship_calc);
+                Session::put('sports', $sports_calc);
+                Session::put('services', $services_calc);
+                Session::put('management', $management_calc);
+
+                return Redirect::to(URL::action('HomeController@getCalculate'));
+        }
+
+        public function getCalculate(){
+                if(!isset($_SERVER['HTTP_REFERER'])){
+                        return Redirect::to(URL::action('HomeController@getAreaQuestions'));
+                }
+                $this->layout           = View::make('calculate');
+                $this->layout->head     = View::make('head');
+        }
+
+        public function getFinish(){
+                $fields = Input::all();
+                arsort($fields);
+                $labels = array();
+                foreach($fields as $key => $value){
+                        switch ($key) {
+                                case 'pol':
+                                        $labels[$key] = "Poljoprivreda";
+                                        break;
+                                case 'hid':
+                                        $labels[$key] = "Hidrometeorologija";
+                                        break;
+                                case 'hem':
+                                        $labels[$key] = "Hemija";
+                                        break;
+                                case 'tek':
+                                        $labels[$key] = "Tekstil";
+                                        break;
+                                case 'sum':
+                                        $labels[$key] = "Šumarstvo";
+                                        break;
+                                case 'geo':
+                                        $labels[$key] = "Geodezija";
+                                        break;
+                                case 'mas':
+                                        $labels[$key] = "Mašinstvo";
+                                        break;
+                                case 'eth':
+                                        $labels[$key] = "Elektrotehnika";
+                                        break;
+                                case 'kul':
+                                        $labels[$key] = "Kultura";
+                                        break;
+                                case 'sao':
+                                        $labels[$key] = "Saobraćaj";
+                                        break;
+                                case 'usl':
+                                        $labels[$key] = "Lične usluge";
+                                        break;
+                                case 'zdr':
+                                        $labels[$key] = "Zdravstvo";
+                                        break;
+                                case 'tru':
+                                        $labels[$key] = "Trgovina i ugostiteljstvo";
+                                        break;
+                                case 'ekp':
+                                        $labels[$key] = "Ekonomija i pravo";
+                                        break;
+                                
+                                default:
+                                        # code...
+                                        break;
+                        }
+                }
+                foreach ($fields as $key => $value) {
+                        echo "<p>".$labels[$key].": ".round($value, 2)."</p><br/>";
+                }
+                
+        }
+
+}
---
+++ app/views/index.blade.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        @yield('head')
+    </head>
+    <body class="skin-blue">
+        <!-- header logo: style can be found in header.less -->
+        <header class="header">
+            <a href="../../index.html" class="logo">
+                <!-- Add the class icon to your logo image or logo icon to add the margining -->
+                {{$title}}
+            </a>
+            <!-- Header Navbar: style can be found in header.less -->
+            <nav class="navbar navbar-static-top" role="navigation">
+                <!-- Sidebar toggle button-->
+                <a href="#" class="navbar-btn sidebar-toggle" data-toggle="offcanvas" role="button">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </a>
+                <div class="navbar-right">
+                    
+                </div>
+            </nav>
+        </header>
+        <div class="wrapper row-offcanvas row-offcanvas-left">
+            <!-- Left side column. contains the logo and sidebar -->
+            <aside class="left-side sidebar-offcanvas">
+                <!-- sidebar: style can be found in sidebar.less -->
+                <section class="sidebar">
+                    <!-- Sidebar user panel -->
+                    <div class="user-panel">
+                        <div class="pull-left image">
+                            <!-- <img src="../../img/avatar3.png" class="img-circle" alt="User Image" /> -->
+                        </div>
+                        <div class="pull-left info">
+                           <!--  <p>Hello, Jane</p> -->
+
+                            <!-- <a href="#"><i class="fa fa-circle text-success"></i> Online</a> -->
+                        </div>
+                    </div>
+                    <!-- search form -->
+                    <!-- <form action="#" method="get" class="sidebar-form">
+                        <div class="input-group">
+                            <input type="text" name="q" class="form-control" placeholder="Search..."/>
+                            <span class="input-group-btn">
+                                <button type='submit' name='seach' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i></button>
+                            </span>
+                        </div>
+                    </form> -->
+                    <!-- /.search form -->
+                    <!-- sidebar menu: : style can be found in sidebar.less -->
+                    <ul class="sidebar-menu">
+                        <!-- <li>
+                            <a href="../../index.html">
+                                <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="../widgets.html">
+                                <i class="fa fa-th"></i> <span>Widgets</span> <small class="badge pull-right bg-green">new</small>
+                            </a>
+                        </li>
+                        <li class="treeview">
+                            <a href="#">
+                                <i class="fa fa-bar-chart-o"></i>
+                                <span>Charts</span>
+                                <i class="fa fa-angle-left pull-right"></i>
+                            </a>
+                            <ul class="treeview-menu">
+                                <li><a href="../charts/morris.html"><i class="fa fa-angle-double-right"></i> Morris</a></li>
+                                <li><a href="../charts/flot.html"><i class="fa fa-angle-double-right"></i> Flot</a></li>
+                                <li><a href="../charts/inline.html"><i class="fa fa-angle-double-right"></i> Inline charts</a></li>
+                            </ul>
+                        </li> -->
+                    </ul>
+                </section>
+                <!-- /.sidebar -->
+            </aside>
+
+            <!-- Right side column. Contains the navbar and content of the page -->
+            <aside class="right-side">
+                <!-- Content Header (Page header) -->
+                <section class="content-header">
+                    <h1>
+                        Pitanja
+                        <small>{{$question_type}}</small>
+                    </h1>
+                </section>
+
+                <!-- Main content -->
+                <section class="content">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="box box-primary">
+                                <div class="box-header">
+                                    <h3 class="box-title">Pitanja {{$question_type}}</h3>
+                                </div><!-- /.box-header -->
+                                <form action="{{URL::action($action)}}" method="post">
+                                    <div class="box-body">
+                                        <p>Na pitanja se odgovara na skali od 1 do 10. Ocena 1 (skroz levo) znači da se najmanje slažete sa postavljenom tvrdnjom, a 10 (skroz desno) da se najviše slažete sa njom.</p>
+                                        <div class="row margin">
+                                            <div class="col-sm-6">
+                                                @foreach($json as $q)
+                                                    <p>{{$q->question}}</p>
+                                                    <input type="text" value="5" name="{{$q->id}}" class="slider form-control" data-slider-min="0" data-slider-max="10" data-slider-step="1" data-slider-value="5" data-slider-orientation="horizontal" data-slider-selection="before" data-slider-tooltip="show" data-slider-id="red">
+                                                    <hr/>
+                                                @endforeach
+                                            </div>
+                                            <div class="col-sm-6">
+                                                <input type="submit" name="submit" value="Pošalji" class="btn btn-primary pull-right">
+                                            </div> 
+                                        </div>
+                                        <input type="submit" name="submit" value="Pošalji" class="btn btn-primary">
+                                    </div><!-- /.box-body -->
+                                </form>
+                            </div><!-- /.box -->
+                        </div><!-- /.col -->
+                    </div><!-- /.row -->
+                </section><!-- /.content -->
+            </aside><!-- /.right-side -->
+        </div><!-- ./wrapper -->
+
+
+        <!-- jQuery 2.0.2 -->
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+        <!-- Bootstrap -->
+        <script src="{{Request::root()}}/assets/js/bootstrap.min.js" type="text/javascript"></script>
+        <!-- AdminLTE App -->
+        <script src="{{Request::root()}}/assets/js/AdminLTE/app.js" type="text/javascript"></script>
+
+        <!-- Bootstrap slider -->
+        <script src="{{Request::root()}}/assets/js/plugins/bootstrap-slider/bootstrap-slider.js" type="text/javascript"></script>
+
+        <script type="text/javascript">
+            $(function() {
+                /* BOOTSTRAP SLIDER */
+                $('.slider').slider();
+            });
+        </script>
+
+    </body>
+</html>
